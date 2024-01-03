dd1986057d59364e8b8aed3fa01dda6650bbbdab
xoxosos <85667817+xoxosos@users.noreply.github.com>
2024-01-03 21:23:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-challenges
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/xoxosos/vue-challenges)
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Inspector from 'vite-plugin-vue-inspector';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    Inspector({
+      enabled: false,
+      toggleButtonVisibility: 'never'
+    }),
+  ],
+});
